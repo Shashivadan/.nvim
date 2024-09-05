28142ffde5cd1dc80b9ac14216623e37c7e1f7c4
Shashivadan <shashivadan99@gmail.com>
2024-09-05 22:11:06 +0530

    feat: some config

---
+++ lua/plugins/Codeium.lua
@@ -0,0 +1,7 @@
+return {
+  "Exafunction/codeium.nvim",
+  cmd = "Codeium",
+  build = ":Codeium Auth",
+  opts = {},
+}
+-- Remove the `use` here if you're using folke/lazy.nvim.
--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,9 +1,10 @@
 return {
-  {
-    "windwp/nvim-ts-autotag",
-    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact", "html", "jsx", "tsx" },
-    config = function()
-      require("nvim-ts-autotag").setup()
-    end,
-  },
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end,
 }
