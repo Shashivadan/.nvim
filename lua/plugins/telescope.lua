b1ed56d11ade0f0f7a65fb4d96e5cb6179f7ff4e
Shashivadan <shashivadan99@gmail.com>
2024-12-01 13:26:37 +0530

    feat: added termnial default intsert mode

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,21 @@
 return {
-  -- amongst your other plugins
   {
     "akinsho/toggleterm.nvim",
     version = "*",
     opts = {
       -- Your custom configuration options go here
+      start_in_insert = true,  -- Automatically start in insert mode
     },
     config = function(_, opts)
       require("toggleterm").setup(opts)
-
       -- Key mapping for \
-      vim.keymap.set("n", "\\", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
+      vim.keymap.set("n", "\\", function()
+        vim.cmd("ToggleTerm")
+        -- Ensure we enter insert mode after opening the terminal
+        vim.cmd("startinsert")
+      end, { noremap = true, silent = true })
+      
+      -- Similar mapping for terminal mode, but exit to normal mode first
       vim.keymap.set("t", "\\", "<C-\\><C-n><cmd>ToggleTerm<cr>", { noremap = true, silent = true })
     end,
   },
