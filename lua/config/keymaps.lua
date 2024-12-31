7368d10eef23230d266be55b2a3de4f973c2fd3d
Shashivadan <shashivadan99@gmail.com>
2024-12-31 22:30:00 +0530

    feat: config update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps heroe
+--
+local map = LazyVim.safe_keymap_set
+
+map("n", "<C-q>", "<C-w>q", { desc = "Close window" })
