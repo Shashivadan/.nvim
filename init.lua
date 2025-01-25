51b250ab1541db5bd41bb4fac5c36efe9e925eb8
Shashivadan <shashivadan99@gmail.com>
2025-01-25 14:18:05 +0530

    Initial commit

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+--
+-- if vim.g.neovide then
+--   -- Refresh rate
+--   vim.g.neovide_refresh_rate = 120
+--   -- Transparency
+--   vim.g.neovide_transparency = 0.8
+--   -- Font
+--   vim.o.guifont = "JetBrains Mono:h12"
+--   -- Cursor animation (decreased for faster animation)
+--   vim.g.neovide_cursor_animation_length = 0.08
+--   -- Scale factor (makes everything smaller, default: 1.0)
+--   vim.g.neovide_scale_factor = 0.8
+--   -- Allow ctrl + scroll to change font size
+--   vim.g.neovide_scale_factor_step = 0.1
+-- end
